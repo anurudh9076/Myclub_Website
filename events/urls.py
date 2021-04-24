5db2619f8acfb6b8b9d6b6e5e36577dfb44aea56
Anurudh <dzariozyaire@gmail.com>
2021-04-24 19:38:41 +0530

    project initialized and did some code

---
+++ events/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path,include
+from . import views
+
+
+urlpatterns = [
+    #path converters
+    #int: numbers
+    #str: strings
+    #path:whole urls /
+    #slug: hypehn-and_underscore_stuff
+    #UUID: universally unique indentifier
+    path('',views.home,name="home"),
+    path('<int:year>/<str:month>/',views.home,name="home"),
+]
