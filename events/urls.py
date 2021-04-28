17f9fabf4ace968a596bd5599e3712bac3633982
anurudh9076 <dzariozyaire@gmail.com>
2021-04-28 18:08:19 +0530

    search and list venue functionality added

--- events/urls.py
+++ events/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('<int:year>/<str:month>/',views.home,name="home"),
     path('events',views.all_events,name='events-list'),
     path('add_venue',views.add_venue,name='add-venue'),
+    path('list_venue',views.list_venue,name='list-venue'),
+    path('show_venue/<venue_id>',views.show_venue,name='show-venue'),
+    path('search_venues',views.search_venues,name='search-venues'),
 ]
